c58428af6910f8fde8a936e6db0ca1de5af46e30
Lee,Byung-Chul <io2oi@yahoo.co.kr>
2017-11-03 00:12:23 +0900

    Addition of the timer.sh for my home training

---
+++ etc/timer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo -n "Duration?: "
+read duration
+echo -n "Rest?: "
+read rest
+
+function prtBanner() {
+    banner "$1 $2" && sleep 1
+    clear
+}
+function du() {
+    for __ii__ in `seq 1 $1`; do
+	prtBanner "Go!" $__ii__
+    done
+    paplay /usr/share/sounds/ubuntu/notifications/Amsterdam.ogg
+}
+function re() {
+    for __ii__ in `seq 1 $1`; do
+	prtBanner "Rest!" $__ii__
+    done
+    paplay /usr/share/sounds/ubuntu/notifications/Mallet.ogg
+}
+function start() {
+    clear
+    prtBanner "Ready!" 5 
+    for __ii__ in `seq 4 -1 1`; do
+	prtBanner "Ready!" $__ii__
+    done
+    paplay /usr/share/sounds/ubuntu/notifications/Mallet.ogg
+}
+
+start
+clear
+for ii in `seq 3 -1 1`; do
+    du $duration
+    clear
+    if [ $ii -ne 1 ]; then
+	re $rest
+	clear
+    fi
+done
