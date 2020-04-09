dc3a95b1dbacfc33fcbd9efeda9476619e5bf660
Byung-Chul Lee <bclee@3billion.io>
2020-04-08 21:14:46 -0400

    modifying codes for using pickling

--- socket_server_client/client.py
+++ socket_server_client/client.py
@@ -1,24 +1,27 @@
+import pickle
 import socket
 import timeit
 
-HOST = '127.0.0.1'
-PORT = 9999
+if __name__=="__main__":
+    HOST = '127.0.0.1'
+    PORT = 9999
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-client_socket.connect((HOST, PORT)) 
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    client_socket.connect((HOST, PORT)) 
 
 
-# 키보드로 입력한 문자열을 서버로 전송하고 
-# 서버에서 에코되어 돌아오는 메시지를 받으면 화면에 출력합니다. 
-# quit를 입력할 때 까지 반복합니다. 
-while True: 
-    message = input('Enter Message : ')
-    if message == 'quit':
-    	break
-    start = timeit.default_timer()
-    client_socket.send(message.encode()) 
-    data = client_socket.recv(1024) 
-    end = timeit.default_timer()
-    print(f"Received from the server : {repr(data.decode())} at {end-start} sec.") 
+    # 키보드로 입력한 문자열을 서버로 전송하고 
+    # 서버에서 에코되어 돌아오는 메시지를 받으면 화면에 출력합니다. 
+    # quit를 입력할 때 까지 반복합니다. 
+    while True: 
+        message = input('Enter Message : ')
+        if message == 'quit':
+            break
+        start = timeit.default_timer()
+        client_socket.send(message.encode()) 
+        data = client_socket.recv(1024)
+        data = pickle.loads(data)
+        end = timeit.default_timer()
+        print(f"Received from the server : {repr(data)} at {end-start} sec.") 
 
-client_socket.close() 
+    client_socket.close()
--- socket_server_client/server.py
+++ socket_server_client/server.py
@@ -2,7 +2,8 @@
 The original example of multi threaded socket is from
 https://webnautes.tistory.com/1381
 '''
-import socket 
+import socket
+import pickle
 from _thread import *
 
 def mock_db(query_key):
@@ -10,15 +11,16 @@ def mock_db(query_key):
     This function has a role of DBMS.
     '''
     dic = {
-        'a':"1234",
-        'b':"4576"
+        '1-1-A-T_HP:000001': [3,10,1,10000],
+        '1-1-A-T_HP:000002': [3,2000, 0,100000]
     }
-    res = dic.get(query_key,"False")
-    return res.encode()
+    res = dic.get(query_key,False)
+    return res
     
-# 쓰레드에서 실행되는 코드입니다.
-# 접속한 클라이언트마다 새로운 쓰레드가 생성되어 통신을 하게 됩니다.
-def threaded(client_socket, addr): 
+def threaded(client_socket, addr):
+    '''
+    supporting multi clients by thread
+    '''
     print('Connected by :', addr[0], ':', addr[1]) 
     # 클라이언트가 접속을 끊을 때 까지 반복합니다. 
     while True: 
@@ -31,27 +33,29 @@ def threaded(client_socket, addr):
             print('Received from ' + addr[0],':',addr[1] , data.decode())
             #client_socket.send(data) 
             print(f"return value: {mock_db(data.decode())}")
-            client_socket.send(mock_db(data.decode()))
+            rt_val = mock_db(data.decode())
+            client_socket.send(pickle.dumps(rt_val))
         except ConnectionResetError as e:
             print('Disconnected by ' + addr[0],':',addr[1])
             break
     client_socket.close() 
 
-HOST = '127.0.0.1'
-PORT = 9999
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    PORT = 9999
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT)) 
-server_socket.listen() 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT)) 
+    server_socket.listen() 
 
-print('server start')
+    print('server start')
 
-# 클라이언트가 접속하면 accept 함수에서 새로운 소켓을 리턴합니다.
-# 새로운 쓰레드에서 해당 소켓을 사용하여 통신을 하게 됩니다. 
-while True: 
-    print('wait')
-    client_socket, addr = server_socket.accept() 
-    start_new_thread(threaded, (client_socket, addr)) 
+    # 클라이언트가 접속하면 accept 함수에서 새로운 소켓을 리턴합니다.
+    # 새로운 쓰레드에서 해당 소켓을 사용하여 통신을 하게 됩니다. 
+    while True: 
+        print('wait')
+        client_socket, addr = server_socket.accept() 
+        start_new_thread(threaded, (client_socket, addr)) 
 
-server_socket.close() 
+    server_socket.close() 
