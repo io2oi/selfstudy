ae213c159b4479aeb3d69925b60fbaadd1c7d7a2
Byung-Chul <io2oi@yahoo.co.kr>
2020-08-13 15:22:40 +0900

    issue #1 : running a program with Pool for multiprocessing

---
+++ multiprocessing/run_withpool.py
@@ -0,0 +1,22 @@
+from multiprocessing import Pool
+import time
+
+def run_withpool(worker, data: list, num_threads: int) -> list:
+    res = list()
+    with Pool(num_threads) as pool_handle:
+        res = pool_handle.map(worker, data)
+    return res
+
+def worker(arg1):
+    time.sleep(5)
+    res = arg1 + 1
+    return res
+
+def main():
+    n_thr = 3
+    input = list(range(6))
+    output = run_withpool(worker, input, 3)
+    print(output)
+    
+if __name__=="__main__":
+    main()
